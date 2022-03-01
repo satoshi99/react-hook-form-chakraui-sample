7458eef534b35722363afa32f7209e845c305054
satoshi <3104nit@gmail.com>
2022-03-01 16:07:35 +0900

    fix form

--- src/components/FormPage.tsx
+++ src/components/FormPage.tsx
@@ -13,21 +13,33 @@ import {
   Input,
   InputGroup,
   InputLeftAddon,
+  InputLeftElement,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Radio,
   RadioGroup,
   Select,
   Stack,
+  Text,
   Textarea,
+  useDisclosure,
 } from '@chakra-ui/react'
-import { useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { ColorModeSwitcher } from '../ColorModeSwitcher'
 import { PREFECTURES } from '../const'
 import { useState } from 'react'
 import 'react-datepicker/dist/react-datepicker.css'
+import { HiOutlineMail, HiOutlinePhone } from 'react-icons/hi'
 
 type FormInputs = {
   name: string
+  contact: 'Email' | 'Phone'
   email: string
+  phone: string
   postalCode: string
   prefecture: string
   adress: string
@@ -42,24 +54,38 @@ type FormInputs = {
 export const FormPage = () => {
   const {
     register,
+    watch,
+    getValues,
+    setValue,
     handleSubmit,
     formState: { errors, isValid, isSubmitting, isSubmitted },
-  } = useForm({
+  } = useForm<FormInputs>({
     mode: 'all',
+    defaultValues: {
+      contact: 'Email',
+      confirm: false,
+    },
   })
 
-  const sleep = (ms: number) =>
-    new Promise((resolve) => setTimeout(resolve, ms))
+  const [values, setValues] = useState<FormInputs>()
 
-  const onSubmit = async (data: FormInputs) => {
-    await sleep(1000)
+  const contactArray = ['Email', 'Phone']
+  const watchContact = watch('contact')
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) =>
     alert(JSON.stringify(data))
-  }
 
   const genderArray = ['Male', 'Female', 'Diverse']
   const [gender, setGender] = useState('')
 
   const optionArray = ['option1', 'option2', 'option3', 'option4', 'option5']
+
+  const onClick = () => {
+    setValues(getValues())
+    onOpen()
+  }
 
   if (isSubmitted)
     return (
@@ -91,23 +117,84 @@ export const FormPage = () => {
               </FormErrorMessage>
             </FormControl>
 
-            <FormControl isRequired isInvalid={errors.email ? true : false}>
-              <FormLabel>Email</FormLabel>
-              <Input
-                type="email"
-                placeholder="guest@example.com"
-                {...register('email', {
-                  required: { value: true, message: 'Email is requried' },
-                  pattern: {
-                    value:
-                      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    message: 'Invalid Email adress',
-                  },
-                })}
-              />
-              <FormErrorMessage>
-                {errors.email && errors.email.message}
-              </FormErrorMessage>
+            <FormControl isRequired isInvalid={errors.contact ? true : false}>
+              <FormLabel>Contact</FormLabel>
+              <RadioGroup value={watchContact} mb={3}>
+                <Stack direction="row" spacing={10}>
+                  {contactArray.map((v, i) => (
+                    <Radio
+                      key={i}
+                      value={v}
+                      isChecked={
+                        watchContact ? v === watchContact : v === 'Email'
+                      }
+                      {...register('contact', {
+                        required: {
+                          value: true,
+                          message: 'Contact is required',
+                        },
+                      })}
+                    >
+                      {v}
+                    </Radio>
+                  ))}
+                </Stack>
+              </RadioGroup>
+              {watchContact === 'Email' ? (
+                <FormControl isRequired isInvalid={errors.email ? true : false}>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<HiOutlineMail />}
+                    />
+                    <Input
+                      type="email"
+                      placeholder="guest@example.com"
+                      {...register('email', {
+                        required: {
+                          value: true,
+                          message: 'Email or Phone number is requried',
+                        },
+                        pattern: {
+                          value:
+                            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                          message: 'Invalid Email adress',
+                        },
+                      })}
+                    />
+                  </InputGroup>
+                  <FormErrorMessage>
+                    {errors.email && errors.email.message}
+                  </FormErrorMessage>
+                </FormControl>
+              ) : (
+                <FormControl isRequired isInvalid={errors.phone ? true : false}>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<HiOutlinePhone />}
+                    />
+                    <Input
+                      type="tel"
+                      placeholder="09011223344"
+                      {...register('phone', {
+                        required: {
+                          value: true,
+                          message: 'Email or Phone number is requried',
+                        },
+                        pattern: {
+                          value: /[0-9]{10,11}/,
+                          message: 'Phone number must be 10 or 11 digits',
+                        },
+                      })}
+                      maxLength={11}
+                    />
+                  </InputGroup>
+                  <FormErrorMessage>
+                    {errors.phone && errors.phone.message}
+                  </FormErrorMessage>
+                </FormControl>
+              )}
             </FormControl>
 
             <FormControl isInvalid={errors.postalCode ? true : false}>
@@ -168,7 +255,7 @@ export const FormPage = () => {
               </FormErrorMessage>
             </FormControl>
 
-            <FormControl isRequired isInvalid={errors.gender ? true : false}>
+            <FormControl>
               <FormLabel>Gender</FormLabel>
               <RadioGroup value={gender} onChange={(value) => setGender(value)}>
                 <Stack direction="row" spacing={10}>
@@ -177,21 +264,13 @@ export const FormPage = () => {
                       key={i}
                       value={v}
                       isChecked={v === gender}
-                      {...register('gender', {
-                        required: {
-                          value: true,
-                          message: 'Gender selection is required',
-                        },
-                      })}
+                      {...register('gender')}
                     >
                       {v}
                     </Radio>
                   ))}
                 </Stack>
               </RadioGroup>
-              <FormErrorMessage>
-                {errors.gender && errors.gender.message}
-              </FormErrorMessage>
             </FormControl>
 
             <FormControl>
@@ -248,15 +327,52 @@ export const FormPage = () => {
               </FormErrorMessage>
             </FormControl>
             <Button
-              type="submit"
+              type="button"
               w="100%"
               colorScheme="teal"
               disabled={!isValid}
-              isLoading={isSubmitting}
+              onClick={onClick}
             >
-              Submit
+              CONFIRM
             </Button>
           </Stack>
+
+          <Modal
+            isCentered
+            onClose={onClose}
+            isOpen={isOpen}
+            scrollBehavior="inside"
+            size="xl"
+          >
+            <ModalOverlay
+              bg="blackAlpha.300"
+              backdropFilter="blur(10px) hue-rotate(90deg)"
+            />
+            <ModalContent>
+              <ModalHeader>Confirm your input</ModalHeader>
+              <ModalBody>
+                <Stack direction="column">
+                  <Text>{values?.name}</Text>
+                  <Text>{values?.email}</Text>
+                  <Text>{values?.adress}</Text>
+                </Stack>
+              </ModalBody>
+              <ModalFooter>
+                <Button type="button" onClick={onClose} mr={3}>
+                  REVISE
+                </Button>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <Button
+                    type="submit"
+                    colorScheme="teal"
+                    isLoading={isSubmitting}
+                  >
+                    SUBMIT
+                  </Button>
+                </form>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </form>
       </Flex>
     </>
